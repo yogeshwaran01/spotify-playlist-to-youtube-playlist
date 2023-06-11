21b35e49df34a8db98a4b0d6703f367262167ce1
YOGESHWARAN R <yogeshin247@gmail.com>
2023-06-11 13:05:19 +0530

    formatted with black; used python-dotenv

--- spotify_client.py
+++ spotify_client.py
@@ -7,26 +7,31 @@ import dotenv
 
 dotenv.load_dotenv()
 
+
 @dataclass
 class Playlist:
     name: str
     description: str
     tracks: list[str]
 
-class SpotifyClient:
 
+class SpotifyClient:
     def __init__(self) -> None:
-        client_id = os.getenv('CLIENT_ID')
-        client_secret = os.getenv('CLIENT_SECRET')
-        auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+        client_id = os.getenv("CLIENT_ID")
+        client_secret = os.getenv("CLIENT_SECRET")
+        auth_manager = SpotifyClientCredentials(
+            client_id=client_id, client_secret=client_secret
+        )
         self.spotify = spotipy.Spotify(auth_manager=auth_manager)
 
     def get_playlist(self, id: str):
         playlist = self.spotify.playlist(id)
         queries = []
-        tracks = playlist['tracks']['items']
+        tracks = playlist["tracks"]["items"]
         for track in tracks:
-            track_name = track['track']['name']
-            artists = ', '.join([artist['name'] for artist in track['track']['artists']])
-            queries.append(f'{track_name} by {artists}')
-        return(Playlist(playlist['name'], playlist['description'], queries))
+            track_name = track["track"]["name"]
+            artists = ", ".join(
+                [artist["name"] for artist in track["track"]["artists"]]
+            )
+            queries.append(f"{track_name} by {artists}")
+        return Playlist(playlist["name"], playlist["description"], queries)
--- youtube_client.py
+++ youtube_client.py
@@ -6,6 +6,7 @@ from googleapiclient.discovery import build
 
 logger.disabled = True
 
+
 class YouTubeClient:
     def __init__(self) -> None:
         flow = InstalledAppFlow.from_client_secrets_file(
@@ -17,7 +18,9 @@ class YouTubeClient:
 
         self.youtube = build("youtube", "v3", credentials=creds)
 
-    def create_playlist(self, name: str, description: str, privacy_status: str = "private"):
+    def create_playlist(
+        self, name: str, description: str, privacy_status: str = "private"
+    ):
         playlist = (
             self.youtube.playlists()
             .insert(
@@ -37,30 +40,26 @@ class YouTubeClient:
         return playlist
 
     def add_song_playlist(self, playlist_id: str, video_id: str):
-        request = (
-                self.youtube.playlistItems()
-                .insert(
-                    part="snippet",
-                    body={
-                        "snippet": {
-                            "playlistId": playlist_id,
-                            "resourceId": {"kind": "youtube#video", "videoId": video_id},
-                        }
-                    },
-                )
-            )
+        request = self.youtube.playlistItems().insert(
+            part="snippet",
+            body={
+                "snippet": {
+                    "playlistId": playlist_id,
+                    "resourceId": {"kind": "youtube#video", "videoId": video_id},
+                }
+            },
+        )
         playlist_item = request.execute()
         return playlist_item
-    
+
     def remove_song_playlist(self, playlist_id: str, video_id: str):
         request = self.youtube.playlistItems().delete(id=video_id)
         response = request.execute()
         return response
 
     def search_video(self, query: str):
-
         return Search(query).results[0]
-    
+
     def get_playlist(self, playlist_id):
         videos = []
         next_page_token = None
@@ -70,7 +69,7 @@ class YouTubeClient:
                 part="snippet",
                 playlistId=playlist_id,
                 maxResults=50,
-                pageToken=next_page_token
+                pageToken=next_page_token,
             )
 
             response = request.execute()
